bee35147000104fe00a5feab784669f25bd328f4
Parul Gera <pgera@innow8apps.com>
2025-05-01 15:02:22 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+//here to connect, we get the application string from the mongodb website
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to mongoDB')
+}).catch((err) => { 
+    console.log(err)
+})
+// we cannot use the process.env.MONGO environment variabdirectly inside the mongoose package, we need to install package dotenv
 
 const app = express()
 
